291cb80e7bec20d9a13e44eb606766d018d420bc
Inna Didenko <innadidenko29@gmail.com>
2024-01-15 23:12:57 +0200

    create swiper

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/NOLA">
-      <App />
-    </BrowserRouter>
+    <div className="container">
+      <BrowserRouter basename="/NOLA">
+        <App />
+      </BrowserRouter>
+    </div>
   </React.StrictMode>
 );
-
